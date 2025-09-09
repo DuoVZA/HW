53ee80024542ed8b7a2f42c5ed7de37f78c75f0f
DuoVZA <zayvlad2009@gmail.com>
2025-09-09 13:33:49 +0300

    hi

---
+++ Task 1/main.js
@@ -0,0 +1,93 @@
+// задание 1
+
+// let age = parseInt(prompt("Введите свой возраст: ", "18"));
+// if (age <12) {
+//     alert("Серьезно? Ты ребенок? Иди учись!!!");
+// }
+// else if (age <18 && age >=12) {
+//     alert("Подросток, иди учись еще!");
+// }
+// else if (age <60 && age >=18) {
+//     alert("Уже другое дело, можем нормально поболтать с тобой, взрослый :)");
+// }
+// else {
+//     alert("Эх, пенсия, ужасная вещь :(");
+// }
+
+// задание 2
+
+// let choise = parseInt(prompt("Введите своё число (0-9):"))
+// switch (choise)
+// {
+//     case 0:
+//         alert(")");
+//         break;
+//     case 1:
+//         alert("!");
+//         break;
+//     case 2:
+//         alert("@");
+//         break;
+//     case 3:
+//         alert("#");
+//         break;
+//     case 4:
+//         alert("$");
+//         break;
+//     case 5:
+//         alert("%");
+//         break;     
+//     case 6:
+//         alert("^");
+//         break;
+//     case 7:
+//         alert("&");
+//         break;
+//     case 8:
+//         alert("*");
+//         break;
+//     case 9:
+//         alert("(");
+//         break;       
+// }
+
+// задание 3
+
+// let number = parseInt(prompt("Введите трехзначное число: ", "123"));
+// let num1 = parseInt(number % 10);
+// let num2 = parseInt(number / 10 % 10);
+// let num3 = parseInt(number / 100);
+// if (num1 == num2 || num2 == num3 || num1 == num3) {
+//     alert("Число имеет две одинаковые цифры");
+// }
+// else
+// {
+//     alert("Число не имеет одинаковые цифры");
+// }
+
+// задание 4
+
+// let year = parseInt(prompt("Введите год: ", "2025"));
+// (year % 400 === 0 || year % 4 === 0 && year% 100 !== 0 ) ? alert("Год высокосный") : alert("Год не высокосный");
+
+// задание 5
+
+
+
+// задание 6
+
+
+
+// задание 7
+
+
+
+// задание 8
+
+
+
+// задание 9
+
+
+
+// задание 10
